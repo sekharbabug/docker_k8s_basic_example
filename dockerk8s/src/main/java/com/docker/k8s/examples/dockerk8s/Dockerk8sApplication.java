3bbfe5da533e5bd72d2525bb58f297a18dd1759d
sekharbabug <sekhar.babu260@gmail.com>
2023-07-30 21:34:05 +0400

    Update Dockerk8sApplication.java

--- dockerk8s/src/main/java/com/docker/k8s/examples/dockerk8s/Dockerk8sApplication.java
+++ dockerk8s/src/main/java/com/docker/k8s/examples/dockerk8s/Dockerk8sApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ComponentScan("com.docker.k8s.examples")
 public class Dockerk8sApplication {
 
 	public static void main(String[] args) {

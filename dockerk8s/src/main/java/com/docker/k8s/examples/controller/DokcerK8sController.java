c160f2ad5f6cb034f1aeb991a1d9eb0d7bb70b56
sekharbabug <sekhar.babu260@gmail.com>
2023-08-12 15:51:07 +0530

    Update DokcerK8sController.java

--- dockerk8s/src/main/java/com/docker/k8s/examples/controller/DokcerK8sController.java
+++ dockerk8s/src/main/java/com/docker/k8s/examples/controller/DokcerK8sController.java
@@ -1,5 +1,11 @@
 package com.docker.k8s.examples.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,5 +19,12 @@ public class DokcerK8sController {
 	@GetMapping("/getMessage")
 	public String getMessageFromK8s() {
 		return "Welcome Docker Container from k8s";
+	}
+
+	@GetMapping("/getData")
+	public ResponseEntity<Object> getMessageData() {
+		List list=Stream.of("laptop", "dell", "sony", "mac").collect(Collectors.toList());
+		return new ResponseEntity<Object>(list, HttpStatus.OK);
+		
 	}
 }

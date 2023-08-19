142b8f38f0f9640cd048d17881db1687d2be1b6c
sekharbabug <sekhar.babu260@gmail.com>
2023-08-19 21:01:44 +0530

    Update DokcerK8sController.java

--- dockerk8s/src/main/java/com/docker/k8s/examples/controller/DokcerK8sController.java
+++ dockerk8s/src/main/java/com/docker/k8s/examples/controller/DokcerK8sController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/dokcerk8s")
+@RequestMapping("/dockerk8s")
 public class DokcerK8sController {
 	
 	
